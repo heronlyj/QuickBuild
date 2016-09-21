08a67e812e161fb1dc10b861eae52ebbfe6d2071
heronlyj <heronlyj@gmail.com>
2016-09-21 10:24:44 +0800

    Initial Commit

---
+++ QuickBuild/QuickBuild.h
@@ -0,0 +1,19 @@
+//
+//  QuickBuild.h
+//  QuickBuild
+//
+//  Created by lyj on 21/09/2016.
+//  Copyright © 2016 heronlyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuickBuild.
+FOUNDATION_EXPORT double QuickBuildVersionNumber;
+
+//! Project version string for QuickBuild.
+FOUNDATION_EXPORT const unsigned char QuickBuildVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuickBuild/PublicHeader.h>
+
+
